5f57e57b4a0aa0ceaa44258e3c9ac16f76092bd8
Your Name <bfaul@bfaul-NFORCE-680i-LT-SLI.(none)>
2014-08-14 15:17:33 -0700

    Delete src

--- src/CPX.h
+++
@@ -1,7 +0,0 @@
-void CPXloop();
-void CPXsetup();
-void CPXaddFunction(char* name, int (*functionPtr)(char*,char*));
-void sendPing();
-void CPXerrorPin(int);
-void CPXokPin(int);
-int CPXconnected();
